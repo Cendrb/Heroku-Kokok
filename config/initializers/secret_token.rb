d079bd68a06d8f6c5fe75d4503a20cf1fcef7d62
Cendrb <cendrb@gmail.com>
2014-01-26 11:15:07 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerokuPorn::Application.config.secret_key_base = '7237362666c4bf570862544e75775dec7b2ca48dc51abcc8b4369ec2802c5ff2c0a45779baa9d3897cf2d3c95f731ad5b6031e20db1daf9d0977ee55ecc1efe7'
